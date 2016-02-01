7f06a4db195725dfd5509874b7d7d493e75b66cc
Matt Menzenski <matt.menzenski@gmail.com>
2016-01-31 21:57:37 -0600

    finish basic to-do list

---
+++ assets/js/todos.js
@@ -0,0 +1,24 @@
+$('#todo-list').on("click", "li", function() {
+    $(this).toggleClass("done");
+});
+
+$('#todo-list').on("click", ".delete", function(e) {
+    $(this).parent().fadeOut(500, function() {
+        $(this).remove();
+    });
+    e.stopPropagation();
+});
+
+$('#add').keypress(function(e) {
+    if (e.which === 13) {
+        var newTodo = $(this).val();
+        var span = '<span class="delete"><i class="fa fa-trash"></i></span> '
+        $(this).val("");
+        $('#todo-list').append('<li>' + span + newTodo +'</li>');
+    }
+});
+
+$('#edit-symbol').click(function() {
+    $(this).toggleClass("fa-plus fa-minus");
+    $('#add').fadeToggle();
+});
